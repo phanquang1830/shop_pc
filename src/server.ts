37ab2f60d04d388efca7dd6c21305e73b66ac9fd
PhanQuang <phanquang1830@gmail.com>
2026-05-25 11:59:43 +0700

    Recfactor: use zod to valitation and remove sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,11 @@
 import app from "./app.js";
-import sequelize from "./config/database.js";
 import env from "./config/env.js";
-
-import "./models/user.model.js";
 
 const PORT = env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    await sequelize.authenticate();
-
     console.log("Database connected");
-
-    await sequelize.sync(); // đọc model, tạo table trong mysql
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
