70cc5387be9d6f5387b818e8b7f65118d5f0f914
PhanQuang <phanquang1830@gmail.com>
2026-05-25 11:55:28 +0700

    Add registerSchema, loginSchema, create RegisterPayload and LoginPayload

--- src/validations/auth.validation.ts
+++ src/validations/auth.validation.ts
@@ -1,26 +1,21 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({ // validate schema
-  username: z
-  .string()
-  .min(3, "Username must be at least 3 characters")
-  .max(30),
+export const registerSchema = z.object({
+  // validate schema
+  username: z.string().min(3, "Username must be at least 3 characters").max(30),
 
-  email: z
-  .email("Invalid email"),
+  email: z.email("Invalid email"),
 
-  password: z
-  .string()
-  .min(6, "Password must be at least 6 characters")
+  password: z.string().min(6, "Password must be at least 6 characters"),
 });
-
 
 export const loginSchema = z.object({
-    email: z
-    .email("Email invalid"),
+  email: z.email("Email invalid"),
 
-
-    password: z
-    .string()
-    .min(6, "Password must be at least 6 characters")
+  password: z.string().min(6, "Password must be at least 6 characters"),
 });
+
+// Tự tạo type từ schema
+export type RegisterPayload = z.infer<typeof registerSchema>;
+
+export type LoginPayload = z.infer<typeof loginSchema>;
