f19e16bb61a5342dfe5d44dd46782e23d09b2c92
PhanQuang <phanquang1830@gmail.com>
2026-05-27 12:11:37 +0700

    Add productRouter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import authRoute from "./routes/auth.route.js";
+import productRouter from "./routes/product.route.js";
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
+app.use("/api/products", productRouter);
 
 export default app;
